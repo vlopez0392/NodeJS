db47869db4a4ac3829505077916da7f616e6ba1a
Victor D. Lopez <vlopez0392@gmail.com>
2024-02-09 02:35:48 +0800

    Event emitters with parameters

---
+++ Section3/38_Node_Event_Emitter/events_params.js
@@ -0,0 +1,21 @@
+const EventEmitter = require('events');
+const celebrity = new EventEmitter();
+
+//Subscribe celebrity for Observer 1
+celebrity.on('race', (result)=>{
+    if(result === 'win'){
+        console.log('Congratulations! Your are great');
+    }
+});
+
+//Subscribe celebrity for Observer 2
+celebrity.on('race', (result)=>{
+    if(result === 'win'){
+        console.log('Boo, I could have done better than that!');
+    }
+});
+
+//Name of the event is passed to the emit function
+celebrity.emit('race', 'win');
+celebrity.emit('race', 'lost'); //No listeners for this event
+celebrity.emit('race', 'win');
